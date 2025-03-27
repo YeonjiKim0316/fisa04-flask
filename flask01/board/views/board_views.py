75a9ae163bf47ac5f1f2713d8f198bfa2746e621
YeonjiKim0316 <kimyeonji3@gmail.com>
2025-03-27 11:57:17 +0900

    add render_template

--- flask01/board/views/board_views.py
+++ flask01/board/views/board_views.py
@@ -1,10 +1,15 @@
 
 # Blueprint 기능을 사용해서 collection/no2/
-from flask import Blueprint
+from flask import Blueprint, render_template
 from ..models import Question
 
 cbp = Blueprint('collection', __name__, url_prefix='/board')
 
+    
+@cbp.route('/boardlist')
+def list():
+    return render_template('board_list.html', question)
+    
 # Blueprint 기능을 사용해서 collection/no1/
 @cbp.route('/no1')
 def hello2():

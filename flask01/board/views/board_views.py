b7fa9fe3e5af5fcaff2b223b71f023d6857c6abe
YeonjiKim0316 <kimyeonji3@gmail.com>
2025-03-27 16:41:10 +0900

    made extends

--- flask01/board/views/board_views.py
+++ flask01/board/views/board_views.py
@@ -1,9 +1,9 @@
 
-# Blueprint 기능을 사용해서 collection/no2/
+# Blueprint 기능을 사용
 from flask import Blueprint, render_template
 from ..models import Question
 
-cbp = Blueprint('collection', __name__, url_prefix='/board')
+cbp = Blueprint('board', __name__, url_prefix='/board')
 
 
 # templates 디렉토리 안에 들어있는 file 경로를 읽고, view에 작성한 객체를 달아서 렌더링해서 전달
@@ -14,7 +14,7 @@ def list():
     return render_template('board/boardList.html', question_list=question)
 
 # 개별 게시글을 조회할 수 있는 함수
-@cbp.route('/detail/<int:question_id>/')
+@cbp.route('/details/<int:question_id>/')
 def detail(question_id):
     # get_or_404() 메서드로 값을 조회하면 404에러를 발생시킵니다.
     # question = Question.query.get(question_id)
